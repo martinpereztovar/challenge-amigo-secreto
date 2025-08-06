a2023ba29a0c2d5313e4c9ca06b54545deb61880
Martin <martinpereztovar@gmail.com>
2025-08-06 09:44:24 -0300

    funcao atualizar lista implementada

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 let nomeDoAmigo = document.getElementById("amigo");
+let listaDeAmigos = document.getElementById("listaAmigos");
 
 function adicionarAmigo() {
   if (nomeDoAmigo.value.length < 1) {
@@ -10,4 +11,12 @@ function adicionarAmigo() {
   }
 
   nomeDoAmigo.value = "";
+  atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+  listaDeAmigos.innerHTML = "";
+  amigos.forEach((nome) => {
+    listaDeAmigos.innerHTML += `<li>${nome}</li>`;
+  });
 }
